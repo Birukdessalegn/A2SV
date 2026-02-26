b7eb2cefb7efa7af916baf58a6aa4adb3d01f142
Biruk Dessalegn <birukdessalegn123@gmail.com>
2026-02-26 21:25:57 +0300

    Add solution for Sorting: Bubble Sort | HackerRank (trial 4)

---
+++ hackerrank/sorting-bubble-sort-hackerrank_trial4.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    # Write your code here
+    nums = len(a)
+    swap = 0
+    for i in range (nums):
+        for j in range(0 , nums-i-1):
+            if a[j] > a[j+1]:
+                a[j] , a[j+1] = a[j+1] , a[j]
+                swap += 1
+    print(f"Array is sorted in {swap} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[-1]}")
+                
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
